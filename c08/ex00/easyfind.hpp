c94f9e5931cc41779c498126966afb3dfec483dc
Aceek <linhardtilan@gmail.com>
2023-07-03 21:26:09 +0200

    add first c09/ex00 > easyfind function

---
+++ c08/ex00/easyfind.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilinhard <ilinhard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/03 20:04:00 by ilinhard          #+#    #+#             */
+/*   Updated: 2023/07/03 21:25:33 by ilinhard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _C08_EX00_EASYFIND_HPP_
+#define _C08_EX00_EASYFIND_HPP_
+
+#include <iostream>
+#include <climits>
+#include <vector>
+#include <algorithm>
+#include <list>
+#include <map>
+
+template <typename T>
+typename T::iterator easyfind(T &container, int value) {
+	typename T::iterator it = std::find(container.begin(), container.end(), value);
+	if (it == container.end()) {
+		std::cout << "Value not found in container" << std::endl;
+		return (container.end());
+	}
+	std::cout << "Value find in container" << std::endl;
+	return (it);
+}
+
+#endif  // _C08_EX00_EASYFIND_HPP_
---
+++ c08/ex00/main.cpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilinhard <ilinhard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/03 17:54:21 by ilinhard          #+#    #+#             */
+/*   Updated: 2023/07/03 21:25:35 by ilinhard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+
+bool	ft_atoi(char *string, int *nb) {
+	int			sign = 1;
+	int			i = 0;
+	long int	res = 0;
+	int			max = 0;
+
+	if (string[i] == '+' || string[i] == '-')
+	{
+		if (string[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (string[i] >= '0' && string[i] <= '9') {
+		res = res * 10 + (string[i] - '0');
+		max++;
+		i++;
+	}
+	res *= sign;
+	if (string[i] != '\0' || max > 10 || i == 0
+			|| (i == 1 && (string[0] == '+' || string[0] == '-'))
+			|| res > INT_MAX || res < INT_MIN)
+		return (false);
+
+	*nb = res;
+	return (true);
+}
+
+int	main(int ac, char **av) {
+
+	int		number = 0;
+	bool	valid = false;
+	
+
+	if (ac != 2) {
+		std::cout << "Put number to find in container, max one value" << std::endl;
+		return (EXIT_FAILURE);
+	}
+	valid = ft_atoi(av[1], &number);
+	if (!valid) {
+		std::cout << "Arg not valid int number, format : ./easyfind [INT_NUMBER] " << std::endl;
+		return (EXIT_FAILURE);
+	}
+
+	int	array[] = {0, 1, 2, 8, 9, 12, 15, -2147483648};
+	std::vector<int> test(array, array + sizeof(array) / sizeof(array[0]));
+	std::list<int> test2(array, array + sizeof(array) / sizeof(array[0]));
+
+	easyfind(test, number);
+	easyfind(test2, number);
+
+	return (EXIT_SUCCESS);
+}
